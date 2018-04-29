9806afd8ee7760b510e4367c1b9e476ffb14db0b
Remlik <Remlikmg@comcast.net>
2018-04-29 02:53:39 -0400

    Ant Prefab under construction

---
+++ AntColonySim/Assets/Scripts/Ant/Ant_Legs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant_Legs : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			Debug.Log ('!');
+			rb.velocity = transform.right * -1 * speed;
+		} else {
+			rb.velocity = transform.right * 0;
+		}
+	}
+}
